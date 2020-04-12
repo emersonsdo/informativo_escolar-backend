9eee1cae448b6676894c94c7474014bf116d8ebb
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-04-12 19:37:36 -0300

    Descobrindo funcionalidades do moment

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,27 +1,47 @@
 //store, index (quais tipos de filtros serão oferecidos?), show, update, destroy
 const moment = require('moment');
+const User = require('../models/User');
 
 module.exports = {
 
-    store(req, res) {
+    async store(req, res) {
         const { name } = req.body;
         const { cpf }  = req.body;
         const { email } = req.body;
         const { pin } = req.body;
         const { dependents } = req.body;
+        const active = true;
+        const created_at = moment.now();
 
-        console.log(`Nome: ${name} \n CPF: ${cpf} \n E-mail: ${email} \n Pin: ${pin} \n`);
+        const user = await User({
+            name,
+            cpf,
+            email,
+            pin,
+            dependents,
+            active,
+            created_at
+        });
 
+        /*console.log(`Nome: ${name} \n CPF: ${cpf} \n E-mail: ${email} \n Pin: ${pin} \n`);
         console.log('Dados do(s) dependente(s): \n');
         Object.keys(dependents).forEach(function(key) {
-            // var birth_date = moment(dependents[key].birth_date, "DD-M-YYYY");
+            var birth_date = moment(dependents[key].birth_date, 'DD/MM/YYYY');
+            var day = birth_date.format('DD');
+            var month = birth_date.format('MM');
+            var year = birth_date.format('YYYY');
             console.log(`Nome: ${dependents[key].name}
             Matrícula: ${dependents[key].register_number} 
             Data de nascimento: ${dependents[key].birth_date}
+            Dia de nascimento: ${day}
+            Mês de nascimento: ${month}
+            Ano de nascimento: ${year}
             Série: ${dependents[key].grade}`);
-        });
 
-        return res.json({name: `${name}`});
+            
+        });*/
+
+        return res.json(user);
     }
 
 }
