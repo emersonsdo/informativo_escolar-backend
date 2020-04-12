e7307436c8e90ad05356b60cfbd96b2f5dd1abb4
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-04-12 13:56:10 -0300

    Pegando dados recebidos na chamada rest para criar um usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,8 +5,23 @@ module.exports = {
 
     store(req, res) {
         const { name } = req.body;
+        const { cpf }  = req.body;
+        const { email } = req.body;
+        const { pin } = req.body;
+        const { dependents } = req.body;
 
-        return res.json({message: 'Hello Wolrd'});
+        console.log(`Nome: ${name} \n CPF: ${cpf} \n E-mail: ${email} \n Pin: ${pin} \n`);
+
+        console.log('Dados do(s) dependente(s): \n');
+        Object.keys(dependents).forEach(function(key) {
+            // var birth_date = moment(dependents[key].birth_date, "DD-M-YYYY");
+            console.log(`Nome: ${dependents[key].name}
+            Matrícula: ${dependents[key].register_number} 
+            Data de nascimento: ${dependents[key].birth_date}
+            Série: ${dependents[key].grade}`);
+        });
+
+        return res.json({name: `${name}`});
     }
 
-};
+}
