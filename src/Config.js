a4211dfa0576bd5704f328b44bdadef8d499735e
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-04-29 18:25:55 -0300

    Delete Config.js

--- src/Config.js
+++
@@ -1,5 +0,0 @@
-//Texto: 'aplicativo CEEB informa'
-const jwtSecret = "338f489dbbd1baecfe27e12d49b45428d5e5d535ce0f0b43e9217216e7941725d234a3c0aeeaa6e2610ed938d659df58ce23de163764332cb6b341e9fdce6fde";
-const anotherConfig = "ADM";
-
-module.exports = {jwtSecret, anotherConfig};
